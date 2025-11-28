575fabc91e404656a51ed03c72697cabcb01a5c0
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2025-11-28 22:20:52 +0100

    Update cluster.rb

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -2,6 +2,7 @@ class Cluster < ApplicationRecord
   include CheckConflicts
 
   audited associated_with: :import
+  attr_accessor :import # used by audited associated_with: :import
 
   has_many :areas
 end
