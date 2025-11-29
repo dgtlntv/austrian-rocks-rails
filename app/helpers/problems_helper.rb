3f30f484a9e81af4ac4da8c19a0ba97867caacd8
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2025-11-29 19:37:43 +0100

    more fixes

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -2,4 +2,9 @@ module ProblemsHelper
   def problem_friendly_path(problem)
     area_problem_path(problem.area, problem)
   end
+
+  def circle_view(content, klass: "h-6 w-6 leading-6")
+    content_tag(:span, content,
+      class: "rounded-full #{klass} inline-flex justify-center bg-brand-500")
+  end
 end
