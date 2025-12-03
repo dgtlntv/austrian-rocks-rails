b20f989d5e3bb7b5d832e0a4d4b20817f4b9a03e
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2025-12-03 22:40:59 +0100

    update video link display

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -7,4 +7,24 @@ module ProblemsHelper
     content_tag(:span, content,
       class: "rounded-full #{klass} inline-flex justify-center bg-brand-500")
   end
+
+  def video_platform_info(url)
+    uri = URI.parse(url)
+    host = uri.host.to_s.downcase
+
+    case host
+    when /youtube\.com/, /youtu\.be/
+      { name: "YouTube", icon: "youtube", color: "red" }
+    when /vimeo\.com/
+      { name: "Vimeo", icon: "vimeo", color: "blue" }
+    when /instagram\.com/
+      { name: "Instagram", icon: "instagram", color: "pink" }
+    when /tiktok\.com/
+      { name: "TikTok", icon: "tiktok", color: "black" }
+    else
+      { name: "Video", icon: "play", color: "gray" }
+    end
+  rescue URI::InvalidURIError
+    { name: "Video", icon: "play", color: "gray" }
+  end
 end
